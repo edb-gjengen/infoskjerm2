d797d370ca20635e0c0155aa2697239ba979ed12
Nikolai R Kristiansen <nikolaik@gmail.com>
2016-08-06 14:59:14 +0200

    staticfiles

--- infoskjerm2/settings.py
+++ infoskjerm2/settings.py
@@ -116,6 +116,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Local settings
 try:
     from .local_settings import *
